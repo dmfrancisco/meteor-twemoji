b2650ce6e7e7917eca5b8e8998b4178ff9085de2
SeriousM <masta2ooo@gmail.com>
2014-08-06 12:34:25 +0000

    Updated the code to work with meteor 0.8.3

--- emoji.js
+++ emoji.js
@@ -6,32 +6,31 @@ Emoji.convert = function (str) {
     }
 
     return str.replace(/:[\+\-a-z0-9_]+:/gi, function(match) {
-        var imgName = match.slice(1, -1);
-        var path = '/packages/emoji-continued/img/' + imgName + '.png';
+        var imgName = match.slice(1, -1),
+            path = '/packages/emoji-continued/img/' + imgName + '.png';
         return "<img class='emoji' title='" + match + "' src='" + path + "'/>";
     });
 };
 
 // borrowed code from https://github.com/meteor/meteor/blob/devel/packages/showdown/template-integration.js
-if (Package.ui) {
-  var UI = Package.ui.UI;
-  var HTML = Package.htmljs.HTML; // implied by `ui`
-  Package.ui.UI.registerHelper('emoji', UI.block(function () {
-    var self = this;
-    
-    if (self.__content){
+if (Package.templating) {
+  var Template = Package.templating.Template,
+      UI = Package.ui.UI, // implied by `templating`
+      HTML = Package.htmljs.HTML, // implied by `ui`
+      Blaze = Package.blaze.Blaze; // implied by `ui`
+
+  UI.registerHelper("emoji", Template.__create__('emoji', function () {
+    var view = this,
+        content = '';
+
+    if (view.templateContentBlock) {
       // this is for the block usage eg: {{#emoji}}:smile:{{/emoji}}
-      return function () {
-        var text = UI.toRawText(self.__content, self /*parentComponent*/);
-        return HTML.Raw(Emoji.convert(text));
-      };
+      content = Blaze.toText(view.templateContentBlock, HTML.TEXTMODE.STRING);
     }
-    else
-    {
-      // this is for the direct usage eg: {{> emoji ":smile:"}}
-      return function(){
-        return HTML.Raw(Emoji.convert(self.parent.data()));
-      };
+    else {
+      // this is for the direct usage eg: {{> emoji ":blush:"}}
+      content = view.parentView.dataVar.get();
     }
+    return HTML.Raw(Emoji.convert(content));
   }));
 }
